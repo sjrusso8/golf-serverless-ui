3bfdc56302b8aae1122ebab33a8c30e9d4e8aa84
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2021-01-31 08:50:07 -0500

    chart packages

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from "./store";
-import Highcharts from 'highcharts';
-import VueHighcharts from 'vue-highcharts';
 import './assets/tailwind.css'
 
 createApp(App)
     .use(router)
     .use(store)
-    .use(VueHighcharts, { Highcharts })
     .mount('#app')
