5ace10ed11328b2a6c8bd52267714d8333467726
sjrusso8 <sjrusso8@gmail.com>
2021-02-01 01:44:36 +0000

    gh-pages

---
+++ precache-manifest.679c4eb3ed622cd427aec75c34c08411.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d057d02c27e74b42b29",
+    "url": "/golf-serverless-ui/css/app.d658cc3a.css"
+  },
+  {
+    "revision": "18bab0a278971717f2d2dbf45835e306",
+    "url": "/golf-serverless-ui/img/golf-course-cover.jpg"
+  },
+  {
+    "revision": "45901f705b3e49aba39266cd7be6e6b5",
+    "url": "/golf-serverless-ui/img/headshot_golfer.png"
+  },
+  {
+    "revision": "105cc7a5336622fe267fd359462777be",
+    "url": "/golf-serverless-ui/index.html"
+  },
+  {
+    "revision": "8d057d02c27e74b42b29",
+    "url": "/golf-serverless-ui/js/app.86476a8e.js"
+  },
+  {
+    "revision": "5379f2833cd3996cc557",
+    "url": "/golf-serverless-ui/js/chunk-2d22996d.c5eb1b91.js"
+  },
+  {
+    "revision": "d9954f49db9429b800b9",
+    "url": "/golf-serverless-ui/js/chunk-vendors.78cb375d.js"
+  },
+  {
+    "revision": "3c5efb86dd4f9f422737",
+    "url": "/golf-serverless-ui/js/course.6c287468.js"
+  },
+  {
+    "revision": "1f1f997217b0ab252ede",
+    "url": "/golf-serverless-ui/js/coursedetails.bf8c8a55.js"
+  },
+  {
+    "revision": "e0cac4aff0e84c38c7fa",
+    "url": "/golf-serverless-ui/js/dashboard.bf228b8d.js"
+  },
+  {
+    "revision": "88b0c983472c2659c602",
+    "url": "/golf-serverless-ui/js/logout.346d194e.js"
+  },
+  {
+    "revision": "205ef1b99a3768cf50ae",
+    "url": "/golf-serverless-ui/js/practice.988374e5.js"
+  },
+  {
+    "revision": "096fe06c3e53d0af6277",
+    "url": "/golf-serverless-ui/js/profile.638d55c3.js"
+  },
+  {
+    "revision": "e0e56b0e4ea2da48d7de",
+    "url": "/golf-serverless-ui/js/rounddetails.0ff8d0b8.js"
+  },
+  {
+    "revision": "d0ab5686813daaaa1a57",
+    "url": "/golf-serverless-ui/js/rounds.59e8939b.js"
+  },
+  {
+    "revision": "1e4ab47d85058ae7d76d8dfe0ab6053b",
+    "url": "/golf-serverless-ui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/golf-serverless-ui/robots.txt"
+  }
+]);
